df8da55a90f0b7f10675045ce5d29fcd79c6ccb0
Tarcísio Marinho de Oliveira <tarcisio.marinho@xpi.com.br>
2021-03-07 12:10:00 -0300

    Removing duplicated code

--- Crypto.Dotnet/Core/Symmetric/GCM.cs
+++ Crypto.Dotnet/Core/Symmetric/GCM.cs
@@ -8,64 +8,8 @@ using System.Security.Cryptography;
 
 namespace Crypto.Dotnet.Core.Symmetric
 {
-    internal class GCM : IDisposable
+    internal static class GCM
     {
-        private byte[] _key;
-        private byte[] _nonce;
-        private int keySize = 32;
-        private int nonceSize = 12;
-
-        public GCM()
-        {
-
-        }
-
-        public GCM(byte[] key)
-        {
-            _key = key;
-            _key = SHA256Hash.Hash(_key);
-            _nonce = new byte[nonceSize];
-            _nonce = RandomBytes.Generate(nonceSize);
-        }
-
-        public GCM(string key)
-        {
-            _key = ByteConversion.StringToByte(key);
-            _key = SHA256Hash.Hash(_key);
-            _nonce = new byte[nonceSize];
-            _nonce = RandomBytes.Generate(nonceSize);
-        }
-
-        // TODO: validate
-        public byte[] Encrypt(byte[] data)
-        {
-            byte[] tag = new byte[keySize];
-            byte[] cipherText = new byte[data.Length];
-
-            using (var cipher = new AesGcm(_key))
-            {
-                cipher.Encrypt(_nonce, data, cipherText, tag, null);
-                return ByteConversion.Concat(tag, ByteConversion.Concat(_nonce, cipherText));
-            }
-        }
-
-        // TODO: validate
-        public byte[] Decrypt(byte[] data)
-        {
-            byte[] tag = ByteConversion.SubArray(data, 0, keySize);
-            byte[] nonce = ByteConversion.SubArray(data, keySize, nonceSize);
-
-            byte[] toDecrypt = ByteConversion.SubArray(data, keySize + nonceSize, data.Length - tag.Length - nonce.Length);
-            byte[] decryptedData = new byte[toDecrypt.Length];
-
-            using (var cipher = new AesGcm(_key))
-            {
-                cipher.Decrypt(nonce, toDecrypt, tag, decryptedData, null);
-
-                return decryptedData;
-            }
-        }
-
         public static byte[] Encrypt(byte[] data, byte[] key)
         {
             var nonceSize = AesGcm.NonceByteSizes.MinSize;
@@ -84,7 +28,6 @@ namespace Crypto.Dotnet.Core.Symmetric
             }
         }
 
-        // TODO: validate
         public static byte[] Decrypt(byte[] data, byte[] key)
         {
             var tagSize = AesGcm.TagByteSizes.MinSize;
@@ -102,11 +45,6 @@ namespace Crypto.Dotnet.Core.Symmetric
 
                 return decryptedData;
             }
-        }
-
-        public void Dispose()
-        {
-
         }
     }
 }
